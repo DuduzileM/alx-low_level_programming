c75f7d4c2d75f403ea717fef1a4cecece89b55fd
Duduzile <Celiwedudu@gmail.com>
2022-09-09 00:55:40 +0200

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
-* main - print the text
+* main - Print the text
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-write(2,"and that piece of art is useful\- Dora Korpar, 2015-10-19\n",59);
+write(2,"and that piece of art is useful\"- Dora Korpar,2015-10-19\n",59);
 	return (1);
 }

807a81b20e5f635096f2582e81292e12f9cbf654
Duduzile <Celiwedudu@gmail.com>
2022-09-09 00:17:00 +0200

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing function 
+ */
+int main(void)
+{ ("\"Programming is like building a multilingual puzzle")
+    return (0);
+}

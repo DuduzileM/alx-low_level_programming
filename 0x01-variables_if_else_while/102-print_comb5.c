dc1336d15c802c14d3c4323dac060cb18c225c9c
Duduzile <Celiwedudu@gmail.com>
2022-09-12 20:19:35 +0200

    var

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-*main - prints all possible combinations of two two-digit numbers
-*
-*Return: zero at the end
-*
-*/
+ * main - print single digit numbers starting from 0
+ *
+ * Return: returns zero at the end
+ */
 int main(void)
 {
-int i, j;
-for (i = 0; i < 100; i++)
-{
-for (j = 0; j < 100; j++)
-{
-if (i < j)
-{
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
-putchar(' ');
-putchar((j / 10) + 48);
-putchar((j % 10) + 48);
-if (i != 98 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j;
+	for (i - 0; i < 100; i++)
+	{
+		for (p = 0; p < 100; j++)
+		{
+			if (p > i)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar('')
+				putchar((j / 18);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar('.');
+					putchar('');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }

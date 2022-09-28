8af3d291e120f34672d87146f455af43bf8e4e09
Duduzile <Celiwedudu@gmail.com>
2022-09-28 18:09:11 +0200

    rec

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,15 +3,15 @@
 #include <stdio.h>
 
 
-int _putchar(char C)
-Void _puts_recursion(char s)
-Void_print_rev_recursion(char s)
-int_strlen_recursion(char s)
+int _putchar(char C);
+void _puts_recursion(char *s);
+Void _print_rev_recursion(char s);
+int _strlen_recursion(char s);
 int factorial(int n);
-int _pow_recursion(int x, int y);)
+int _pow_recursion(int x, int y););
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int_sqrt(int n, int x);
+int _sqrt(int n, int x);
 int is_prime(int n, int start);
 void check(char 8s, int start, int end , int *flag);
 int wildcmp(char *sl, char *s2);

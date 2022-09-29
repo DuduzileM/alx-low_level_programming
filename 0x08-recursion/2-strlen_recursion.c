4f5adcb2c6e0ddb42752713b70203e78aeb66de3
Duduzile <Celiwedudu@gmail.com>
2022-09-29 11:06:34 +0200

    rec

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,24 @@
 #include "main.h"
 
- /** 
-  * _strlen_recursion - note: no _putchar in local test 
-  * @s: input 
-  * Return: Always 0 (Success) 
-  */ 
- int _strlen_recursion(char *s) 
- { 
-  if (*s == '\0') 
-                 return (0); 
-         else 
-                 return (1 + _strlen_recursion(s + 1)); 
- }
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: int/ length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(++s);
+		return (len);
+	}
+	return (0);
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,41 @@
-#include "main.h" 
- int prime_checker(int n, int i); 
- /** 
-  * is_prime_number - executes prime_checker 
-  * @n: input to check 
-  * Return: Always 0 (Success) 
-  */ 
- int is_prime_number(int n) 
- { 
-         if (n <= 1) 
-                 return (0); 
-         else if (prime_checker(n, n / 2) > 0) 
-                 return (1); 
-         return (0); 
- } 
-  
- /** 
-  * prime_checker - checks for prime 
-  * @n: input to check 
-  * @i: n / 2, then passes to i - 1, checks if greater than 0 
-  * Return: prime check 
-  */ 
- int prime_checker(int n, int i) 
- { 
-         if (i == 1) 
-                 return (1); 
-         if (n % i == 0) 
-                 return (0); 
-         else 
-                 return (prime_checker(n, i - 1)); 
- }
+#include "main.h"
+
+/**
+  * _isitprime - looks for prime
+  * @n: the number
+  * @i: the number / 2
+  *
+  * Return: 1 if prime, 0 if not
+  */
+
+int _isitprime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (_isitprime(n, i - 1));
+	}
+}
+
+/**
+ * is_prime_number - looks for prime
+ * @n: the number
+ * Return: 1 if orime, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n % 2 == 0)
+		return (0);
+	else
+		return (_isitprime(n, n / 2));
+
+}

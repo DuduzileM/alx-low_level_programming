d76adf77cd822f74970aa5957e98434b3aaf7cd1
Duduzile <Celiwedudu@gmail.com>
2022-09-29 09:49:32 +0200

    rec

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,9 +5,9 @@
   * @s: input 
   * Return: Always 0 (Success) 
   */ 
- int _strlen_recursion(char *s) 
+ int _strlen_recursion(char *s) 
  { 
-         if (*s == '\0') 
+  if (*s == '\0') 
                  return (0); 
          else 
                  return (1 + _strlen_recursion(s + 1)); 

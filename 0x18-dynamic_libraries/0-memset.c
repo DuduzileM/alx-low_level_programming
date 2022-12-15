6e3fd1bfcea2c6e92bacb7935d2ce1cee6d657ef
Duduzile <Celiwedudu@gmail.com>
2022-12-15 18:47:30 +0200

    commit

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h" 
+
+/** 
+* _memset - this function fills memory block with a constant byte
+* @s: pointed destination
+* @b: constant byte
+* @n: byte
+* Return: Always 0 (success)
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    while (n)
+    {
+        s[n - i] = b;
+        n--;
+    }
+    return(s)
+}
